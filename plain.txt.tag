8560fe25a1c48cc3abe7b62442192a35453f03c9
nlp <nlp@nlp-VirtualBox.(none)>
2015-12-28 18:34:43 +0100

    parsed text results

---
+++ plain.txt.tag
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE cesAna SYSTEM "xcesAnaIPI.dtd">
+<cesAna xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0" type="lex disamb">
+<chunkList>
+</chunkList>
+</cesAna>
--- xmlParser.py
+++ xmlParser.py
@@ -1,4 +1,6 @@
 import xml.etree.ElementTree as ET
+import os
 
-tree = ET.parse('country_data.xml')
+#tree = ET.parse('country_data.xml')
 
+os.system("~/apps/wcrft/wcrft/wcrft.py -d ~/apps/model_nkjp10_wcrft/ ~/apps/wcrft/config/nkjp.ini -i txt ./texts/reymont_chlopi-3 -O ./xmlResults/reymont.xml")
